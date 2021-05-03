770e7f20cb6041f8e8e24bfc90acfcef90dad798
Chance Linz 😎 <chanlinz@amazon.com>
2021-05-03 11:28:39 -0700

    chore(pkg): changed variable name

--- src/main.rs
+++ src/main.rs
@@ -184,13 +184,13 @@ fn play(player_names: &Vec<String>) {
 }
 
 fn main() {
-    let player_1 = get_player_name(1);
-    let player_2 = get_player_name(2);
+    let player_one = get_player_name(1);
+    let player_two = get_player_name(2);
 
     let mut players = vec![];
 
-    players.push(player_1);
-    players.push(player_2);
+    players.push(player_one);
+    players.push(player_two);
 
     play(&players);
 }
